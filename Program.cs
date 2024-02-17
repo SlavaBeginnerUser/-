d7c4b225a0a2fd49e8a408f55912909081ba2d10
Viacheslav Avdeev <slava708@yandex.ru>
2024-02-17 19:03:22 +0300

    добавил 2 варианта решения задачи

--- Program.cs
+++ Program.cs
@@ -5,7 +5,52 @@
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 // Примеры:
-// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
-// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
-// [“Russia”, “Denmark”, “Kazan”] → []
+// ["Hello", "2", "world", ":-)"] → [“2”, “:-)”]
+// ["1234", "1567", "-2", "computer science"] → [“-2”]
+// ["Russia", "Denmark", "Kazan"] → []
 
+// // Решение №1
+// string[] initArray = {"Hello", "2", "world", ":-)"};
+
+// for (int e = 0; e < initArray.Length; e++)
+// {
+// 	if (initArray[e].Length <= 3)
+// 	{
+// 		Console.Write($"{initArray[e]} ");
+// 		Console.WriteLine();
+// 	}
+// }
+
+// Решение №2
+string[] initArray = { "1234", "1567", "-2", "computer science" };
+Console.Write($"Заданный массив: ");
+Console.WriteLine();
+for (int e = 0; e < initArray.Length; e++)
+{
+		Console.Write($"{initArray[e]} ");
+		Console.WriteLine();
+}
+int sizeOfResultArray = 0;
+for (int i = 0; i < initArray.Length; i++)
+{
+		if (initArray[i].Length <= 3)
+		{
+		sizeOfResultArray++;
+		}
+}
+string[] resultArray = new string[sizeOfResultArray];
+int j = 0;
+int h=0;
+Console.Write($"Массив на выходе: ");
+Console.WriteLine();
+while (h < initArray.Length)
+{
+	if (initArray[h].Length <= 3)
+	{
+		resultArray[j] = initArray[h];
+		Console.Write($"{resultArray[j]} ");
+		Console.WriteLine();
+		j++;
+	}
+	h++;
+}
